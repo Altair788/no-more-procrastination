047aa036bdc100cb6116ef1a6fe0450c904c1500
Eduard Slobodyanik <slobodyanik.ed@gmail.com>
2025-02-16 23:59:02 +0300

    update: обновить модель пользователя

--- users/models.py
+++ users/models.py
@@ -1,6 +1,45 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+
+NULLABLE = {"blank": True, "null": True}
+
 
 class User(AbstractUser):
-    pass
+    username = None
 
+    email = models.EmailField(
+        unique=True, verbose_name="почта", help_text="укажите почту"
+    )
+
+    phone = PhoneNumberField(
+        **NULLABLE,
+        unique=True,
+        verbose_name="номер телефона",
+        help_text="укажите телефон"
+    )
+    country = models.CharField(max_length=100, verbose_name="страна", blank=True)
+    tg_nick = models.CharField(
+        max_length=50,
+        verbose_name="телеграм - ник",
+        blank=True,
+        help_text="укажите ник телеграм"
+    )
+    tg_id = models.PositiveIntegerField(
+        verbose_name="телеграм - ID",
+        **NULLABLE,
+        help_text="укажите телеграм - ID"
+    )
+    avatar = models.ImageField(
+        upload_to="users/avatars/",
+        verbose_name="аватар",
+        help_text="Загрузите аватарку",
+        **NULLABLE
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
