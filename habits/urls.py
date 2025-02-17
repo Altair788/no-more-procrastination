294a4eb74bf69ba3eec7485e0a5372104fcd2d11
Eduard Slobodyanik <slobodyanik.ed@gmail.com>
2025-02-17 19:29:51 +0300

    fix: исправить название пути

--- habits/urls.py
+++ habits/urls.py
@@ -7,7 +7,7 @@ from habits.views import HabitViewSet, PublicHabitListApiView
 app_name = HabitsConfig.name
 
 router = DefaultRouter()
-router.register(r'habits', HabitViewSet, basename='habits')
+router.register(r'', HabitViewSet, basename='habit')
 
 urlpatterns = [
     path("public/", PublicHabitListApiView.as_view(), name="public-habits-list")
