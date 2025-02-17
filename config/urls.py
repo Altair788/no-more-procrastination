94ac6a364e942065a203b40ed435b14f89e324fe
Eduard Slobodyanik <slobodyanik.ed@gmail.com>
2025-02-17 15:57:27 +0300

    update: добавить путь к habits

--- config/urls.py
+++ config/urls.py
@@ -21,6 +21,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("users/", include("users.urls", namespace="users")),
+    path("habits/", include("habits.urls", namespace="habits")),
     # path(
     #     "swagger/",
     #     schema_view.with_ui("swagger", cache_timeout=0),
