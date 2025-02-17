c2c48dc71d7d419c9796d6af66908d896fb184ed
Eduard Slobodyanik <slobodyanik.ed@gmail.com>
2025-02-17 17:47:11 +0300

    update: добавить валидатор в сериализатор

--- habits/serializers.py
+++ habits/serializers.py
@@ -1,9 +1,20 @@
 from rest_framework import serializers
 
 from habits.models import Habit
+from habits.validators import HabitValidator
 
 
-class HabitSerializer(serializers.Serializer):
+class HabitSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Habit
         fields = "__all__"
+
+    def validate(self, data):
+        """
+        Применяем кастомный валидатор для проверки бизнес - логики.
+        """
+        validator = HabitValidator()
+        validator(data)
+        return data
+
