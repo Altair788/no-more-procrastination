029c5f356cc48f4dd5a28707d7094c4ab414a349
Eduard Slobodyanik <slobodyanik.ed@gmail.com>
2025-02-17 16:04:51 +0300

    feat: добавить общий контроллер и контроллер дженерик (кастомный)

--- habits/views.py
+++ habits/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from habits.models import Habit
+from habits.paginations import HabitPaginator
+from habits.serializers import HabitSerializer
+
+
+class HabitViewSet(ModelViewSet):
+    queryset = Habit.objects.all()
+    serializer_class = HabitSerializer
+    pagination_class = HabitPaginator
+
+#  Кастомный эндпоинт для публичных привычек
+class PublicHabitListApiView(ListAPIView):
+    queryset = Habit.objects.filter(is_public=True)
+    serializer_class = HabitSerializer
