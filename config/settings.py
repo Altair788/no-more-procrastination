e578cee6aa7f2d42855c7db71c79eea0da3ca6cd
Eduard Slobodyanik <slobodyanik.ed@gmail.com>
2025-02-17 19:14:55 +0300

    feat: установить права доступа: каждый пользователь имеет доступ только к своим привычкам по механизму CRUD; пользователь может видеть список публичных привычек без возможности редактировать и удалять их

--- config/settings.py
+++ config/settings.py
@@ -46,7 +46,8 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
-    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.AllowAny",),
+    # "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.AllowAny",),
+    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
     "DEFAULT_RENDERER_CLASSES": (
         "djangorestframework_camel_case.render.CamelCaseJSONRenderer",
         "djangorestframework_camel_case.render.CamelCaseBrowsableAPIRenderer",
--- habits/views.py
+++ habits/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from rest_framework.generics import ListAPIView
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.exceptions import PermissionDenied
 
 from habits.models import Habit
 from habits.paginations import HabitPaginator
@@ -8,11 +10,46 @@ from habits.serializers import HabitSerializer
 
 
 class HabitViewSet(ModelViewSet):
-    queryset = Habit.objects.all()
+    """
+    Представление для работы с привычками текущего пользователя.
+    """
+
     serializer_class = HabitSerializer
     pagination_class = HabitPaginator
+    permission_classes = [IsAuthenticated]
+
+
+    def get_queryset(self):
+        """
+        Возвращает привычки, принадлежащие текущему пользователю.
+        """
+        return Habit.objects.filter(owner=self.request.user)
+
+    def perform_create(self, serializer):
+        """
+        Устанавливает текущего пользователя как владельца привычки.
+        """
+        serializer.save(owner=self.request.user)
+
+    def perform_update(self, serializer):
+        """
+        Запрещает редактирование публичных привычек.
+        """
+        if self.get_object().is_public:
+            raise PermissionDenied("Вы не можете редактировать публичную привычку.")
+        serializer.save()
+
+    def perform_destroy(self, instance):
+        """
+        Запрещает удаление публичных привычек.
+        """
+        if instance.is_public:
+            raise PermissionDenied("Вы не можете удалять публичную привычку.")
+        instance.delete()
+
 
 #  Кастомный эндпоинт для публичных привычек
 class PublicHabitListApiView(ListAPIView):
     queryset = Habit.objects.filter(is_public=True)
     serializer_class = HabitSerializer
+    permission_classes = [AllowAny]
