f4f150ff2a640afd6017535d76f4ce61a4879c0e
Eduard Slobodyanik <slobodyanik.ed@gmail.com>
2025-02-18 11:58:18 +0300

    update: для обработки поля avatar в модели user добавить маршруты обраьотки (для Postman) медиафайлов в режиме разработки

--- config/urls.py
+++ config/urls.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import include, path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 from drf_yasg import openapi
@@ -38,3 +40,7 @@ urlpatterns = [
 
     path('redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
+
+# Добавляем маршруты для работы с медиафайлами только в режиме разработки
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
