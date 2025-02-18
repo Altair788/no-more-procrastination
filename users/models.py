6746266333e612d7f95fd2382dcd23fc68ae212b
Eduard Slobodyanik <slobodyanik.ed@gmail.com>
2025-02-18 17:30:26 +0300

    feat: для тестирования необходима корректная работа create_user- добавить кастомного менеджера пользователей

--- users/models.py
+++ users/models.py
@@ -1,10 +1,36 @@
 import secrets
 
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import BaseUserManager
 from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
 
 NULLABLE = {"blank": True, "null": True}
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Users must have an email address')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        """Создает и возвращает суперпользователя с заданным email и паролем."""
+        extra_fields.setdefault('is_staff', True)  # Суперпользователь должен быть staff
+        extra_fields.setdefault('is_superuser', True)  # Суперпользователь должен быть суперпользователем
+
+        # Проверяем, что is_staff и is_superuser установлены в True
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Суперпользователь должен иметь is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Суперпользователь должен иметь is_superuser=True.')
+
+        return self.create_user(email, password, **extra_fields)
+
 
 
 class User(AbstractUser):
@@ -49,7 +75,7 @@ class User(AbstractUser):
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
 
-
+    objects = CustomUserManager()
 
     class Meta:
         verbose_name = "Пользователь"
