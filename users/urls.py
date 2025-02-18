d10121d2bb5c0372f17ddf2b9888dfeacc1bf683
Eduard Slobodyanik <slobodyanik.ed@gmail.com>
2025-02-18 12:05:02 +0300

    update: добавить пути для регистрации/аутентификации по email, сброса пароля

--- users/urls.py
+++ users/urls.py
@@ -6,13 +6,18 @@ from rest_framework_simplejwt.views import (TokenObtainPairView,
 from users.apps import UsersConfig
 from users.views import (UserCreateAPIView, UserDestroyAPIView,
                          UserListAPIView, UserRetrieveAPIView,
-                         UserUpdateAPIView)
+                         UserUpdateAPIView, UserRegisterAPIView, PasswordResetAPIView, PasswordResetConfirmAPIView,
+                         EmailVerificationAPIView)
 
 app_name = UsersConfig.name
 
 
 urlpatterns = [
-    path("register/", UserCreateAPIView.as_view(), name="user-register"),
+    # path("register/", UserCreateAPIView.as_view(), name="user-register"),
+    path("register/", UserRegisterAPIView.as_view(), name="register"),
+    path("email-confirm/<str:token>/", EmailVerificationAPIView.as_view(), name="email_confirm"),
+    path("password-reset/", PasswordResetAPIView.as_view(), name="password_reset"),
+    path("password-reset-confirm/", PasswordResetConfirmAPIView.as_view(), name="password_reset_confirm"),
     path("", UserListAPIView.as_view(), name="user-list"),
     path("<int:pk>/", UserRetrieveAPIView.as_view(), name="user-retrieve"),
     path("update/<int:pk>/", UserUpdateAPIView.as_view(), name="user-update"),
