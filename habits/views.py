9d346ba725fd04d5385bfec7f784b3556e7b7ff5
Eduard Slobodyanik <slobodyanik.ed@gmail.com>
2025-03-22 20:04:16 +0300

    feat: api description

--- habits/views.py
+++ habits/views.py
@@ -1,3 +1,4 @@
+from drf_spectacular.utils import extend_schema
 from rest_framework.exceptions import PermissionDenied
 from rest_framework.generics import ListAPIView
 from rest_framework.permissions import AllowAny, IsAuthenticated
@@ -16,6 +17,43 @@ class HabitViewSet(ModelViewSet):
     serializer_class = HabitSerializer
     pagination_class = HabitPaginator
     permission_classes = [IsAuthenticated]
+
+    @extend_schema(
+        description="Получить список привычек текущего пользователя",
+        responses={200: HabitSerializer(many=True)}
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
+    @extend_schema(
+        description="Создать новую привычку",
+        request=HabitSerializer,
+        responses={201: HabitSerializer()}
+    )
+    def create(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
+
+    @extend_schema(
+        description="Получить детальную информацию о привычке",
+        responses={200: HabitSerializer()}
+    )
+    def retrieve(self, request, *args, **kwargs):
+        return super().retrieve(request, *args, **kwargs)
+
+    @extend_schema(
+        description="Обновить существующую привычку",
+        request=HabitSerializer,
+        responses={200: HabitSerializer()}
+    )
+    def update(self, request, *args, **kwargs):
+        return super().update(request, *args, **kwargs)
+
+    @extend_schema(
+        description="Удалить привычку",
+        responses={204: None}
+    )
+    def destroy(self, request, *args, **kwargs):
+        return super().destroy(request, *args, **kwargs)
 
     def get_queryset(self):
         """
@@ -45,6 +83,11 @@ class HabitViewSet(ModelViewSet):
             raise PermissionDenied("Вы не можете удалять публичную привычку.")
         instance.delete()
 
+
+@extend_schema(
+    description="API endpoint для просмотра публичных привычек.",
+    responses={200: HabitSerializer(many=True)}
+)
 
 #  Кастомный эндпоинт для публичных привычек
 class PublicHabitListApiView(ListAPIView):
